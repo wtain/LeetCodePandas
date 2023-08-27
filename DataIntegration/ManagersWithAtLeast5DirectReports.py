1d46aa632bf4416b50b8e08abcb41a41e403a15a
Ramiz Zeynalov <ramiz.zeynalov@gmail.com>
2023-08-27 02:57:54 +0200

    Daily Pandas - yet another 2

---
+++ DataIntegration/ManagersWithAtLeast5DirectReports.py
@@ -0,0 +1,82 @@
+"""
+https://leetcode.com/problems/managers-with-at-least-5-direct-reports/?envType=study-plan-v2&envId=30-days-of-pandas&lang=pythondata
+
+Create table If Not Exists Employee (id int, name varchar(255), department varchar(255), managerId int)
+Truncate table Employee
+insert into Employee (id, name, department, managerId) values ('101', 'John', 'A', 'None')
+insert into Employee (id, name, department, managerId) values ('102', 'Dan', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('103', 'James', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('104', 'Amy', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('105', 'Anne', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('106', 'Ron', 'B', '101')
+
+data = [[101, 'John', 'A', None], [102, 'Dan', 'A', 101], [103, 'James', 'A', 101], [104, 'Amy', 'A', 101], [105, 'Anne', 'A', 101], [106, 'Ron', 'B', 101]]
+Employee = pd.DataFrame(data, columns=['id', 'name', 'department', 'managerId']).astype({'id':'Int64', 'name':'object', 'department':'object', 'managerId':'Int64'})
+
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+
+
+Write a solution to find managers with at least five direct reports.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | None      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output:
++------+
+| name |
++------+
+| John |
++------+
+"""
+
+import pandas as pd
+
+
+# Runtime
+# Details
+# 548ms
+# Beats 6.25%of users with Pandas
+# Memory
+# Details
+# 63.34MB
+# Beats 7.66%of users with Pandas
+def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
+    managers = employee.groupby('managerId').nunique().reset_index().query('id >= 5')[['managerId']]
+    return managers.merge(employee, how='left', left_on='managerId', right_on='id')[['name']].dropna()
+
+
+data = [[101, 'John', 'A', None], [102, 'Dan', 'A', 101], [103, 'James', 'A', 101], [104, 'Amy', 'A', 101], [105, 'Anne', 'A', 101], [106, 'Ron', 'B', 101]]
+Employee = pd.DataFrame(data, columns=['id', 'name', 'department', 'managerId']).astype({'id':'Int64', 'name':'object', 'department':'object', 'managerId':'Int64'})
+
+
+print(find_managers(Employee))
